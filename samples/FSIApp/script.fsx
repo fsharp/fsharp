49f396afccb155a02b54925bce6324900b643f9a
Mohamed Suhaib Fahad <mohamedsuhaiba@live.in>
2012-02-20 19:53:32 +0530

    updated sample to show intellisense added a new sample taken from tomas intellisense project

--- samples/FSIApp/script.fsx
+++ samples/FSIApp/script.fsx
@@ -1,11 +1,8 @@
-﻿(*namespace CodeDomTest
-
-    module Math =
-        let sqr (x:float) = x * x
-        let cube (x:float) = x * x * x*)
-
-let sqr x = x * x
+﻿let sqr x = x * x
 printfn "Sqr of 10 - %d" (sqr 10)
-
 let cube (x:float) = x * x * x
 printfn "Cube - %A" ([1. .. 10.] |> List.map cube)
+
+module Math =
+    let sqr = sqr
+    let cube = cube
