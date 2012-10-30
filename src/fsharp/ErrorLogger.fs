1959e119d7e1675cb7b75a5c397506a81b5e5648
Ben Winkel <funnelweb007@gmail.com>
2012-10-30 10:12:08 +0000

    Revert deliberate build break

--- src/fsharp/ErrorLogger.fs
+++ src/fsharp/ErrorLogger.fs
@@ -10,7 +10,7 @@
 // You must not remove this notice, or any other, from this software.
 //----------------------------------------------------------------------------
 
-mistake
+
 module internal Microsoft.FSharp.Compiler.ErrorLogger
 
 
