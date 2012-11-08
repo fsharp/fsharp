3a1f5187baaf422a98a92001008334311827feb9
Don Syme <dsyme@microsoft.com>
2012-11-09 00:21:07 +0100

    merge update to F# power pack original sources

--- src/assemblyinfo/assemblyinfo.FSharp.Core.dll.fs
+++ src/assemblyinfo/assemblyinfo.FSharp.Core.dll.fs
@@ -4,7 +4,7 @@ open System.Reflection
 [<assembly:AssemblyDescription("FSharp.Core.dll")>]
 [<assembly:AssemblyCompany("Microsoft Corporation")>]
 [<assembly:AssemblyTitle("FSharp.Core.dll")>]
-[<assembly:AssemblyCopyright("\169 Microsoft Corporation.  All rights reserved.")>]
+[<assembly:AssemblyCopyright("\169 Microsoft Corporation.  Apache 2.0 License.")>]
 [<assembly:AssemblyProduct("Microsoft\174 F#")>]
 
 #if PORTABLE
