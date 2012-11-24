c5ccd3d9583ffd2a411a52e224d0c181162e8db0
funnelweb <funnelweb007@gmail.com>
2012-11-24 01:26:17 +0100

    Add build of fsiAnyCpu.exe to Makefile

--- tests/hosting/hosted.fsx
+++ tests/hosting/hosted.fsx
@@ -1,7 +1,7 @@
 ﻿
 
-#r @"../../lib/debug/4.0/FSharp.Compiler.dll"
-#r @"../../lib/debug/4.0/fsiAnyCpu.exe"
+#r @"../../lib/release/4.0/FSharp.Compiler.dll"
+#r @"../../lib/release/4.0/fsiAnyCpu.exe"
 
 open Microsoft.FSharp.Compiler.Interactive.Shell
 
@@ -15,8 +15,8 @@ let stderrStream = new CompilerOutputStream()
 let stderr = new System.IO.StreamWriter(stderrStream)
 
 stdinStream.Add("printfn \"hello world\";;\n")
-stdinStream.Add("printfn \"this is me\";;\n")
 stdinStream.Add("let x = 1;;\n")
+stdinStream.Add("printfn \"this is me, x = %d\" x;;\n\n")
 stdinStream.Add("let x = 1;;\n")
 
 let session = FsiEvaluationSession([| "fsiAnyCpu.exe" |], stdin, stdout, stderr)
