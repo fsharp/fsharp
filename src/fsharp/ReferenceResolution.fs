c60fda24cacb0a4b580bcd58873fae17e3de6a7a
Ben Winkel <funnelweb007@gmail.com>
2012-12-09 23:56:28 +0100

    Another fix to last checkin

--- src/fsharp/ReferenceResolution.fs
+++ src/fsharp/ReferenceResolution.fs
@@ -206,17 +206,17 @@ module internal MSBuildResolver =
 #if BUILDING_WITH_LKG
         ignore targetProcessorArchitecture
 #else       
-
+        let targetedRuntimeVersionValue = typeof<obj>.Assembly.ImageRuntimeVersion
 #if CROSS_PLATFORM_COMPILER 
         // The properties TargetedRuntimeVersion and CopyLocalDependenciesWhenParentReferenceInGac 
         // are not available to the cross-platform compiler since they are Windows only (not defined in the Mono  
         // 4.0 XBuild support). So we only set them if available (to avoid a compile-time dependency). 
         let runningOnMono = try System.Type.GetType("Mono.Runtime") <> null with e-> false         
         if not runningOnMono then  
-            typeof<ResolveAssemblyReference>.InvokeMember("TargetedRuntimeVersion",(BindingFlags.Instance ||| BindingFlags.SetProperty ||| BindingFlags.Public),null,rar,[| box trv |])  |> ignore 
+            typeof<ResolveAssemblyReference>.InvokeMember("TargetedRuntimeVersion",(BindingFlags.Instance ||| BindingFlags.SetProperty ||| BindingFlags.Public),null,rar,[| box targetedRuntimeVersionValue |])  |> ignore 
             typeof<ResolveAssemblyReference>.InvokeMember("CopyLocalDependenciesWhenParentReferenceInGac",(BindingFlags.Instance ||| BindingFlags.SetProperty ||| BindingFlags.Public),null,rar,[| box true |])  |> ignore 
 #else
-        rar.TargetedRuntimeVersion <- typeof<obj>.Assembly.ImageRuntimeVersion
+        rar.TargetedRuntimeVersion <- targetedRuntimeVersionValue
         rar.CopyLocalDependenciesWhenParentReferenceInGac <- true
 #endif
 #endif        
