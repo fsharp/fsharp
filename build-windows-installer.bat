b0751d423af258e3c25eec6dabae794fd7cee172
Quentin Lord <quasilord1@gmail.com>
2013-02-18 17:08:24 +0100

    include default ngen/msbuild location

--- build-windows-installer.bat
+++ build-windows-installer.bat
@@ -1,19 +1,20 @@
 @if "%_echo%"=="" echo off
 setlocal
 
+set COR=C:\Windows\Microsoft.NET\Framework\v4.0.30319
 set WIX=dependencies\wix\3.7
 set CANDLE=%WIX%\candle.exe
 set LIGHT=%WIX%\light.exe
 
 pushd src
-msbuild fsharp-proto-build.proj
-ngen install lib\bootstrap\4.0\fsc.exe
-msbuild fsharp-library-build.proj /p:Configuration=Release &&^
-msbuild fsharp-compiler-build.proj /p:Configuration=Release &&^
-msbuild fsharp-library-build.proj /p:TargetFramework=net20  /p:Configuration=Release &&^
-msbuild fsharp-library-build.proj /p:TargetFramework=mono21 /p:Configuration=Release &&^
-msbuild fsharp-library-build.proj /p:TargetFramework=portable-net45+sl5+win8 /p:Configuration=Release &&^
-msbuild fsharp-library-build.proj /p:TargetFramework=sl5 /p:Configuration=Release
+%COR%\msbuild fsharp-proto-build.proj
+%COR%\ngen install lib\bootstrap\4.0\fsc.exe
+%COR%\msbuild fsharp-library-build.proj /p:Configuration=Release &&^
+%COR%\msbuild fsharp-compiler-build.proj /p:Configuration=Release &&^
+%COR%\msbuild fsharp-library-build.proj /p:TargetFramework=net20  /p:Configuration=Release &&^
+%COR%\msbuild fsharp-library-build.proj /p:TargetFramework=mono21 /p:Configuration=Release &&^
+%COR%\msbuild fsharp-library-build.proj /p:TargetFramework=portable-net45+sl5+win8 /p:Configuration=Release &&^
+%COR%\msbuild fsharp-library-build.proj /p:TargetFramework=sl5 /p:Configuration=Release
 popd 
 %CANDLE%  -ext WixNetFxExtension src\setup\fsharp-side-by-side.wxs
 %LIGHT%  -out fsharp-3.0-side-by-side.msi -ext WixUIExtension -ext WixNetFxExtension -ext WixVsExtension fsharp-side-by-side.wixobj 
