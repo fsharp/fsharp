97302b15fe920c1de14a560a35bf5146180809d3
Don Syme <donsyme@fastmail.fm>
2013-06-15 17:22:14 +0200

    Fix the hosted FsiEvaluationSession sample
    Add AutoFlush=true and set the global Console.In and Console.Out

--- tests/hosting/hosted.fsx
+++ tests/hosting/hosted.fsx
@@ -5,18 +5,28 @@
 
 open Microsoft.FSharp.Compiler.Interactive.Shell
 
+open System
+open System.IO
+open System.Collections.Generic
+
 let stdinStream = new CompilerInputStream()
 let stdin = new System.IO.StreamReader(stdinStream)
 
+
 let stdoutStream = new CompilerOutputStream()
-let stdout = new System.IO.StreamWriter(stdoutStream)
+let stdout = StreamWriter.Synchronized(new System.IO.StreamWriter(stdoutStream, AutoFlush=true))
 
 let stderrStream = new CompilerOutputStream()
-let stderr = new System.IO.StreamWriter(stderrStream)
+let stderr = StreamWriter.Synchronized(new System.IO.StreamWriter(stderrStream, AutoFlush=true))
 
-stdinStream.Add("printfn \"hello world\";;\n")
+System.Console.SetOut stdout
+System.Console.SetError stderr
+
+stdinStream.Add("eprintfn \"writing to error\";;\n")
+stdinStream.Add("printfn \"hello world to out\";;\n")
 stdinStream.Add("let x = 1;;\n")
-stdinStream.Add("printfn \"this is me, x = %d\" x;;\n\n")
+stdinStream.Add("eprintfn \"this is me on error, x = %d\" x;;\n\n")
+stdinStream.Add("printfn \"this is me on out, x = %d\" x;;\n\n")
 stdinStream.Add("let x = 1;;\n")
 
 let session = FsiEvaluationSession([| "fsiAnyCpu.exe" |], stdin, stdout, stderr)
@@ -25,4 +35,10 @@ let session = FsiEvaluationSession([| "fsiAnyCpu.exe" |], stdin, stdout, stderr)
 async { do session.Run() } |> Async.Start
 
 session.Interrupt()
+
+// Wait a bit before executing these lines
+let text1 = stdoutStream.Read()
+let text2 = stderrStream.Read()
+
+
 
