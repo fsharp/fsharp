ecfe612f2ad9d06243cdf8dd48e5a28bfb3c1002
Steven Stewart-Gallus <sstewartgallus00@mylangara.bc.ca>
2013-09-20 13:34:24 -0700

    Make subexpression executable invocation portable

--- autogen.sh
+++ autogen.sh
@@ -2,5 +2,6 @@
 which autoreconf > /dev/null || (echo "Please install autoconf" && exit 1)
 # on OSX autoconf may need a little help with these paths
 aclocal -I /opt/local/share/aclocal -I /usr/local/share/aclocal 2> /dev/null
-SRCDIR="$(cd "$(dirname $0)" && pwd)"
-autoreconf "${SRCDIR}" && "${SRCDIR}"/configure $@
+SRCDIR="`dirname $0`"
+ABS_SRCDIR="`cd \"$SRCDIR\" && pwd`"
+autoreconf "${ABS_SRCDIR}" && "${ABS_SRCDIR}"/configure $@
