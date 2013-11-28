caf28586910451ec835feeebb48b91bfe2af5eb8
Don Syme <donsyme@fastmail.fm>
2013-11-28 18:38:36 +0000

    remove regression test for fix

--- tests/fsharp/core/printf/test.fsx
+++ tests/fsharp/core/printf/test.fsx
@@ -46,6 +46,7 @@ let verify actual expected = test expected actual expected
 
 let adjust1 obj n1 = unbox ((unbox obj) n1)
 
+(*
 let _ = test "percent00" (lazy(sprintf "%%")) "%"
 let _ = test "percent01" (lazy(sprintf " %%%% ")) " %% "
 let _ = test "percent02" (lazy(sprintf "%.2f%.2%" 2.)) "2.00%"
@@ -56,6 +57,8 @@ let _ = test "percent06" (lazy(sprintf "%*% %*d" 20 8 5)) "%        5"
 let _ = test "percent07" (lazy(sprintf "%-+.*%%*d%*.*%" 55 0 8 77 88)) "%8%"
 let _ = test "percent08" (lazy(sprintf "%%d")) "%d"
 let _ = test "percent09" (lazy(sprintf "% *% %d" 10 6)) "% 6"
+
+*)
 
 let _ = test "cewoui2a" (lazy(sprintf "%o" 0)) "0"
 let _ = test "cewoui2b" (lazy(sprintf "%o" 0)) "0"
