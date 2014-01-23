c925a4e90035d0c40e47d3129c6445068f11fb66
7sharp9 <kukulcanenator@googlemail.com>
2014-01-23 20:15:14 +0000

    Added iOS Tests

---
+++ tests/xplat/core/FsharpUnitTestProject/iOSFSharpLanguageTests/iOSFSharpLanguageTestsViewController.fs
@@ -0,0 +1,28 @@
+﻿namespace iOSFSharpLanguageTests
+
+open System
+open System.Drawing
+
+open MonoTouch.Foundation
+open MonoTouch.UIKit
+
+[<Register ("iOSFSharpLanguageTestsViewController")>]
+type iOSFSharpLanguageTestsViewController () =
+    inherit UIViewController ()
+
+    override x.DidReceiveMemoryWarning () =
+        // Releases the view if it doesn't have a superview.
+        base.DidReceiveMemoryWarning ()
+        // Release any cached data, images, etc that aren't in use.
+
+    override x.ViewDidLoad () =
+        base.ViewDidLoad ()
+        // Perform any additional setup after loading the view, typically from a nib.
+
+    override x.ShouldAutorotateToInterfaceOrientation (toInterfaceOrientation) =
+        // Return true for supported orientations
+        if UIDevice.CurrentDevice.UserInterfaceIdiom = UIUserInterfaceIdiom.Phone then
+           toInterfaceOrientation <> UIInterfaceOrientation.PortraitUpsideDown
+        else
+           true
+
