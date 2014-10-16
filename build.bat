86bba87287f242a06c9634cbce4332a76cdc2473
F# Software Foundation <fsgit@fastmail.fm>
2014-10-16 18:47:14 +0100

    Use v4.5 and MSBuild v12.0

--- build.bat
+++ build.bat
@@ -1,10 +1,6 @@
 @echo off
 ::Env
-if %PROCESSOR_ARCHITECTURE%==x86 (
-	set MSBuild="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
-) else (
-	set MSBUILD=%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
-)
+set MSBuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
 
 ::Clean
 del /F /S /Q lib\proto
