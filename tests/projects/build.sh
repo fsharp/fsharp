b681948f594c09b449f33429aa6c326d6c301751
Don Syme <donsyme@fastmail.fm>
2015-08-05 20:29:45 +0100

    Fix install of various dlls

--- tests/projects/build.sh
+++ tests/projects/build.sh
@@ -20,6 +20,18 @@
 (cd Sample_VS2012_FSharp_ConsoleApp_net40_upgraded_VS2013 && xbuild) &&
 (cd Sample_VS2012_FSharp_Portable_Library_upgraded_2013 && xbuild) &&
 (cd Sample_VS2013_FSharp_Portable_Library_net451_adjusted_to_profile78 && xbuild) &&
+(cd Sample_VS2015_FSharp_Console_App_net40 && xbuild) &&
+(cd Sample_VS2015_FSharp_Console_App_net45 && xbuild) &&
+(cd Sample_VS2015_FSharp_Console_App_net451 && xbuild) &&
+(cd Sample_VS2015_FSharp_Console_App_net452 && xbuild) &&
+(cd Sample_VS2015_FSharp_Library_net40 && xbuild) &&
+(cd Sample_VS2015_FSharp_Library_net45 && xbuild) &&
+(cd Sample_VS2015_FSharp_Library_net45_fsharp_30 && xbuild) &&
+(cd Sample_VS2015_FSharp_Library_net45_fsharp_31 && xbuild) &&
+(cd Sample_VS2015_FSharp_Portable7_Library && xbuild) &&
+(cd Sample_VS2015_FSharp_Portable47_Library && xbuild) &&
+(cd Sample_VS2015_FSharp_Portable78_Library && xbuild) &&
+(cd Sample_VS2015_FSharp_Portable259_Library && xbuild) &&
 echo "all projects built successfully"
 
 
