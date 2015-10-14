c9fb8452ea2a1f3939ef43f80249ecb5f9a73e6f
Chet Husk <chusk3@gmail.com>
2015-10-13 19:16:13 -0500

    meh

--- travis-autogen.sh
+++ travis-autogen.sh
@@ -4,7 +4,8 @@ if [[ $TRAVIS_OS_NAME == osx ]];
     then
         monoVer=$(mono --version | head -n 1 | cut -d' ' -f 5)
         prefix="/Library/Frameworks/Mono.framework/Versions/$monoVer";
-    else prefix="/usr";
+    else 
+    	prefix="/usr";
 fi
 
 ./autogen.sh --prefix=$prefix
