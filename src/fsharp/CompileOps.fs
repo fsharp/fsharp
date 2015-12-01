b16abb5f04bae12c2c118739293acf904caa6229
Don Syme <donsyme@fastmail.fm>
2015-12-01 12:08:33 +0000

    add tentative fix for https://github.com/fsharp/fsharp/issues/483

--- src/fsharp/CompileOps.fs
+++ src/fsharp/CompileOps.fs
@@ -4235,9 +4235,16 @@ type TcImports(tcConfigP:TcConfigProvider, initialResolutions:TcAssemblyResoluti
             | None ->      
                                   
                 if tcConfigP.Get().useMonoResolution then
-                    let resolved = [tcConfig.ResolveLibWithDirectories CcuLoadFailureAction.RaiseError assemblyReference |> Option.get]
-                    resolutions <- resolutions.AddResolutionResults resolved
-                    ResultD resolved
+                    let action = 
+                        match mode with 
+                        | ResolveAssemblyReferenceMode.ReportErrors -> CcuLoadFailureAction.RaiseError
+                        | ResolveAssemblyReferenceMode.Speculative -> CcuLoadFailureAction.ReturnNone
+                    match tcConfig.ResolveLibWithDirectories action assemblyReference with 
+                    | Some resolved -> 
+                        resolutions <- resolutions.AddResolutionResults [resolved]
+                        ResultD [resolved]
+                    | None -> 
+                        ErrorD(AssemblyNotResolved(assemblyReference.Text,assemblyReference.Range))
                 else 
                     // This is a previously unencounterd assembly. Resolve it and add it to the list.
                     // But don't cache resolution failures because the assembly may appear on the disk later.
