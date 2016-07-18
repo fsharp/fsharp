0fd520b832c01ae3444d4da03a6c17e479960573
Don Syme <donsyme@fastmail.fm>
2016-07-18 09:55:41 +0100

    use 160629

--- appveyor.ps1
+++ appveyor.ps1
@@ -32,6 +32,6 @@ pack(gi .\FSharp.Core.Nuget\FSharp.Core.nuspec)
 pack(gi .\FSharp.Compiler.Tools.Nuget\FSharp.Compiler.Tools.nuspec)
 
 # Merge the latest known .NET Core FSharp.Core nuget package with the one we build here
-& packages\NupkgMerge.1.0.0.1\tools\NupkgMerge.exe -p FSharp.Core.Nuget\FSharp.Core.$version.nupkg -s packages\Microsoft.FSharp.Core.netcore.1.0.0-alpha-160627\Microsoft.FSharp.Core.netcore.1.0.0-alpha-160627.nupkg -o lib\release\FSharp.Core.$version.nupkg
+& packages\NupkgMerge.1.0.0.1\tools\NupkgMerge.exe -p FSharp.Core.Nuget\FSharp.Core.$version.nupkg -s packages\Microsoft.FSharp.Core.netcore.1.0.0-alpha-160629\Microsoft.FSharp.Core.netcore.1.0.0-alpha-160629.nupkg -o lib\release\FSharp.Core.$version.nupkg
 & copy FSharp.Compiler.Tools.Nuget\*.nupkg lib\release
 
