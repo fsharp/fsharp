def70a582d431a7412a1d6b74d78ebbd8bd463fa
Enrico Sada <enrico@sada.io>
2017-02-13 16:22:59 +0100

    fix file copy

--- appveyor.ps1
+++ appveyor.ps1
@@ -35,6 +35,7 @@ function pack($nuspec){
 pack(gi .\FSharp.Core.Nuget\FSharp.Core.nuspec)
 pack(gi .\FSharp.Compiler.Tools.Nuget\FSharp.Compiler.Tools.nuspec)
 
+mkdir lib\release\
 copy FSharp.Core.Nuget\*.nupkg lib\release
 copy FSharp.Compiler.Tools.Nuget\*.nupkg lib\release
 
