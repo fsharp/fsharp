d7c02b4261759deaa5758ca666e17892e11981a1
Enrico Sada <enrico@sada.io>
2017-02-13 19:05:25 +0100

    use a project.json to publish the fsc as framework dependent .net core 1.0 app

---
+++ lib/netcore/fsc/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            throw new Exception("Should not be used");
+            return 1;
+        }
+    }
+}
