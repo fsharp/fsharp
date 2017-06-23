9de7a855e11a2fd1b1a2457f9ad8194eef06e1b1
Don Syme <dsyme@microsoft.com>
2017-06-23 19:05:38 +0100

    update diagnostics

--- tests/fsharp/core/run-opt.sh
+++ tests/fsharp/core/run-opt.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 echo "Running optimized..." && \
 which fsharpc && \
-ls -xlR `which fsc` && \
+cat `which fsharpc` && \
+ls -xlR `which fsharpc` && \
 echo fsharpc --define:TESTS_AS_APP --define:Portable --define:MONO --define:UNIX --define:TESTS_AS_APP --quotations-debug+ --optimize+ --debug+ ./run-all.fsx -o run-all-opt.exe && \
 fsharpc --define:TESTS_AS_APP --define:Portable --define:MONO --define:UNIX --define:TESTS_AS_APP --quotations-debug+ --optimize+ --debug+ ./run-all.fsx -o run-all-opt.exe && \
 mono ./run-all-opt.exe
