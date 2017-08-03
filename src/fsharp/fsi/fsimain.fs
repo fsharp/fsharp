6be7a3472e37780b83a99175530fb1ad60530d5c
Don Syme <donsyme@fastmail.fm>
2017-08-03 18:01:32 +1000

    fix fsharpi

--- src/fsharp/fsi/fsimain.fs
+++ src/fsharp/fsi/fsimain.fs
@@ -281,7 +281,7 @@ let evaluateSession(argv: string[]) =
                 member __.GetOptionalConsoleReadLine(probe) = getConsoleReadLine(probe) }
 
         // Create the console
-        and fsiSession = FsiEvaluationSession.Create (fsiConfig, argv, Console.In, Console.Out, Console.Error, collectible=false, legacyReferenceResolver=MSBuildReferenceResolver.Resolver)
+        and fsiSession : FsiEvaluationSession = FsiEvaluationSession.Create (fsiConfig, argv, Console.In, Console.Out, Console.Error, collectible=false, legacyReferenceResolver=MSBuildReferenceResolver.Resolver)
 
 
 #if !FX_NO_WINFORMS
